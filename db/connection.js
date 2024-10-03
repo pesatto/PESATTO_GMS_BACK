db2906823e95f7607204f752aac3dde065a5f8b1
pesatto <fernando.ruiz@pesatto.com>
2024-10-03 14:48:29 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1/pesatto_gms', { tls: "false", ssl: "false", replicaSet: 'rs0' });
+    await mongoose.connect('mongodb://127.0.0.1/pesatto_gms', { directConnection:true, tls: "false", ssl: "false", replicaSet: 'rs0' });
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err.message);
