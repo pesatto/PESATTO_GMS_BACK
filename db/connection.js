ecd1f0fff8d200d9ea421226456474ee9aff549e
pesatto <fernando.ruiz@pesatto.com>
2024-10-03 14:23:11 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //mongodb://127.0.0.1:27017/pesgms
-mongoose.connect('mongodb://127.0.0.1/pesatto_gms', { tls: "false", ssl: "false", replicaSet: 'rs0' }).then(() => {
+mongoose.connect('mongodb://127.0.0.1/pesatto_gms', { tls: "false", ssl: "false", replicaSet: 'rs0', useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Failed to connect to MongoDB', err);
